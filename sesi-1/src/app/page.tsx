ec1fd745b7890d7e67f63f4e81817e5afcd7e0ef
Andrew GW <drudruwinata@gmail.com>
2025-05-14 19:49:41 +0700

    ganti nama dan nim

--- sesi-1/src/app/page.tsx
+++ sesi-1/src/app/page.tsx
@@ -14,14 +14,11 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
+           Nama: Andrew Gregory Winata
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            NIM: 2702245583
           </li>
         </ol>
 
